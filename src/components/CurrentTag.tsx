c851ebe4886c1df88a6e18f986e0f8ad5230b013
Ton Huynh <kintonhtt@gmail.com>
2024-11-07 15:14:32 -0800

    fix bug

--- src/components/CurrentTag.tsx
+++ src/components/CurrentTag.tsx
@@ -1,6 +1,6 @@
 export function CurrentTag() {
   return (
-    <span className="rounded-md border border-blue-500 px-2 py-[1px] text-xs text-blue-500">
+    <span className="rounded-full border border-blue-500 px-2 py-[1px] text-xs text-blue-500">
       Current
     </span>
   );
