99770940c2051207ef03bb447dc3ce0998354a10
Ton Huynh <kintonhtt@gmail.com>
2024-11-07 14:04:39 -0800

    remove client time display component

--- src/components/ClientTimeDisplay.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import moment from "moment";
-
-export function ClientTimeDisplay({ className }: { className?: string }) {
-  return (
-    <span className={className}>{moment().format("dddd, MMMM DD, YYYY")}</span>
-  );
-}
