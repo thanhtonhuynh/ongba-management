6b1ba0fc1e27e94f986e11658687ee4815feabc7
Ton Huynh <kintonhtt@gmail.com>
2024-11-15 23:27:50 -0800

    fix bug

--- src/utils/hours-tips.ts
+++ src/utils/hours-tips.ts
@@ -58,7 +58,8 @@ export function getTodayBiweeklyPeriod(): DayRange {
   if (day < 16) {
     return {
       start: new Date(today.getFullYear(), today.getMonth(), 1, 7),
-      end: new Date(today.getFullYear(), today.getMonth(), 15, 7),
+      // end: new Date(today.getFullYear(), today.getMonth(), 15, 7),
+      end: moment(today).tz("America/Vancouver").day(15).toDate(),
     };
   }
 
