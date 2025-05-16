78614353bf00ab977eb4d31e3e6ea7056270c9fb
Ton Huynh <kintonhtt@gmail.com>
2025-05-16 01:22:07 -0700

    fix bug

--- src/components/EmployeeRoleTag.tsx
+++ src/components/EmployeeRoleTag.tsx
@@ -8,10 +8,10 @@ export function EmployeeRoleTag({ role }: EmployeeRoleTagProps) {
   return (
     <span
       className={cn(
-        `select-none rounded-full px-2 py-[2px] text-xs font-medium capitalize`,
+        `rounded-full px-2 py-[2px] text-xs font-medium capitalize select-none`,
         role === "admin" && "bg-muted text-yellow-500",
         role === "manager" && "bg-muted text-blue-500",
-        (role === "chef" || role === "server") && "bg-muted text-primary",
+        (role === "chef" || role === "server") && "bg-muted",
         role === "unassigned role" && "bg-muted text-muted-foreground",
       )}
     >
